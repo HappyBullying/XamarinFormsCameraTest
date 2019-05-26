1f27fc710a3346e1c929b1b296a5813fb15a6c09
Ринат <renich66666622@gmail.com>
2019-05-27 00:50:08 +0300

    example of custom camera renderer xamarin

---
+++ XamarinFormsCameraTest/XamarinFormsCameraTest/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using Plugin.Media;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamarinFormsCameraTest
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        async private void Press1_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new CameraPage());
+        }
+    }
+}
